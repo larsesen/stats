37cec0caa8ee7947ba81c911678e8624090605e7
Lars Nedberg <lars.nedberg@gmail.com>
2018-12-05 20:33:07 +0100

    Minor refactoring

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import MapFileToObjects
 import dto.Match as Match
 
 directory = "goalFiles/"
-file_names = MapFileToObjects.get_matches_from_directory(directory)
+file_names = MapFileToObjects.get_files_from_directory(directory)
 
 for file_name in file_names:
-    match_info, goals = MapFileToObjects.get_goals_from_file(directory + file_name)
+    match_info, goals = MapFileToObjects.get_data_from_file(directory + file_name)
     match = Match.Match(match_info, goals)
     print match
     print "\n"
