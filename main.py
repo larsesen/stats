0bed7780ba5ec5777082d4656ec8e764f8d157d1
Lars Nedberg <lars.nedberg@gmail.com>
2018-12-14 20:03:09 +0100

    Added penalties for all other matches and prints all matches from main

--- main.py
+++ main.py
@@ -5,17 +5,13 @@ import dto.Match as Match
 directory = "goalFiles/"
 file_names = MapFileToObjects.get_files_from_directory(directory)
 
-# for file_name in file_names:
-#     match_info, goals = MapFileToObjects.get_data_from_file(directory + file_name)
-#     match = Match.Match(match_info, goals)
-#     print match
-#     print "\n"
+for file_name in file_names:
+    match_info, goals, penalties = MapFileToObjects.get_data_from_file(directory + file_name)
+    match = Match.Match(match_info, goals, penalties)
+    print match
+    print "\n"
 
-
-file_name = "goalFiles/20180916_lyn_bmil.json"
-
-
-match_info, goals, penalties = MapFileToObjects.get_data_from_file(file_name)
-match = Match.Match(match_info, goals, penalties)
-
-print match
+# todo Separator between periods
+# todo sort matches by date
+# todo goals and penalties are displayed as they are given, not all goals followed by all penalties
+# todo pretty print goals and penalties so that different parameters are lined under each other
