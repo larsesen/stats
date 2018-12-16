4e28a30e40fe5406987805b2818afeaf6577ff75
Lars Nedberg <lars.nedberg@gmail.com>
2018-12-16 18:57:55 +0100

    Printing of match is separated into separate periods

--- dto/Match.py
+++ dto/Match.py
@@ -1,5 +1,4 @@
 import sys
-import MatchEvent
 
 reload(sys)
 sys.setdefaultencoding('utf8')
@@ -15,13 +14,28 @@ class Match(object):
         self.goals = goals
         self.penalties = penalies
 
-
-# todo Put all goals and penalties in a single list and sort. Divide statistics by period
     def __str__(self):
         match_report = "Date: {}\nArena: {}\nHome team: {}\nAway team: {}\nResult: {}\n" \
             .format(self.match_info.date, self.match_info.arena, self.match_info.home_team, self.match_info.away_team,
                     self.match_info.result)
 
+        match_report += self.get_printed_match_events()
+        return match_report
+
+    def get_printed_match_events(self):
+        match_events = self.get_match_events_sorted()
+        text = ""
+        period = 0
+        for match_event in match_events:
+            event_period = match_event.period
+            if event_period > period:
+                text += "\n-----period {}------\n".format(period + 1)
+                period = event_period
+
+            text += match_event.__str__() + "\n"
+        return text
+
+    def get_match_events_sorted(self):
         match_events = []
         for goal in self.goals:
             match_events.append(goal)
@@ -29,12 +43,4 @@ class Match(object):
             match_events.append(penalty)
 
         match_events.sort(key=lambda x: x.sort_stamp)
-
-        # todo find better way than type checks
-        for match_event in match_events:
-            if type(match_event) is MatchEvent.Goal:
-                match_report += match_event.__str__() + "\n"
-            elif type(match_event) is MatchEvent.Penalty:
-                match_report += match_event.__str__() + "\n"
-
-        return match_report
+        return match_events
